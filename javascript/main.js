e5c83f0227dec0c652e126358b44a82679fbbf30
victorhebdo <victor@gabella.org>
2018-09-18 17:19:54 +0200

    MAJ fichier JS avec popup & bouton

--- javascript/main.js
+++ javascript/main.js
@@ -9,31 +9,36 @@ document.querySelector('body').style.backgroundColor = "red";
 
 
 */
-console.log('coucouc')
 
 
-var myP = document.querySelectorAll('p');
-
-myP.forEach(function(item) {
-  item.style.color = 'blue';
-  console.log('coucouc')
-});
-
+setTimeout(function() { if (confirm('Voulez-vous aller sur xxxvidsxxx ? ')) {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+} else {
+    // Do nothing!
+}; }, 5000);
 
 
-/*
+
+
 var monBouton = document.querySelector('button');
 var monFond = document.querySelector('body');
-var monP = document.querySelectorAll('p');
+var myP = document.querySelectorAll('p');
 var mesTitres = document.querySelectorAll('h1,h2,h3');
 
 
-forEach.function changerFond() {
-  monFond.style.backgroundColor = "#456789" ;
-  monP.style.color = '#957390';
-  mesTitres.style.color = '#893282';
-  monP.style.font = 'Arial';
-  mesTitres.style.font = 'Comic Sans';
+function changerFond() {
+  monFond.style.backgroundColor = "magenta" ;
+  myP.forEach(function(item) {
+    item.style.color = 'blue';
+    item.style.fontFamily = 'papyrus';
+    console.log('coucou')
+  });
+  mesTitres.forEach(function(item) {
+    item.style.color = 'green';
+    item.style.fontFamily = 'Comic Sans';
+    console.log('batard')
+  });
+
 }
 
 
@@ -42,17 +47,14 @@ monBouton.onclick = function() {
   changerFond();
 }
 
-var monTitre = document.querySelector('h1');
-monTitre.textContent = 'Hello world!';
-
-
-
-
-
 
 
 
 /*
+var monTitre = document.querySelector('h1');
+monTitre.textContent = 'Hello world!';
+
+
 var monImage = document.getElementByClassName('imgnulle,elmt');
 
 monImage.onclick = function() {
